176ff66974d2c9ea013a7f3688a16ac7d905dfc6
lejolo20 <lejolo20@gmail.com>
2022-06-11 20:33:34 -0400

    pagina con home, destination, crew inicial

--- main.js
+++ main.js
@@ -14,3 +14,18 @@ navToggle.addEventListener("click", ()=>{
     }
 /*     console.log(visibility) */
 })
+
+
+const moon = document.getElementById("moon");
+const mars = document.getElementById("mars")
+
+mars.addEventListener("click",()=>{
+
+            const visibilitySection=moon.getAttribute(aria-hidden)
+            if (visibilitySection=="false")
+                moon.setAttribute("aria-hidden","true")
+            else {
+                moon.setAttribute("aria-hidden","false")
+            }
+
+})
