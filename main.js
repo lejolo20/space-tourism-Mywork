e87c0f8f97cfaab67f06c54b7750d655e8eab59d
lejolo20 <lejolo20@gmail.com>
2022-06-14 22:56:37 -0400

    home con detalle, destination y crew finalizada

--- main.js
+++ main.js
@@ -38,30 +38,31 @@ function changeTabFocus(e) {
 
     if (e.keyCode===keydownLeft || e.keyCode===keydownRight){
         tabs[tabFocus].setAttribute("tabsindex",-1);
-    }
-
-
-    // if the right key is pushed, move to the next tab on the right
-    if  (e.keyCode===keydownRight){
-        tabFocus++
-        if (tabFocus >= tabs.length){
-            tabFocus=0;
+    
+        // if the right key is pushed, move to the next tab on the right
+        if  (e.keyCode===keydownRight){
+            tabFocus++
+            if (tabFocus >= tabs.length){
+                tabFocus=0;
+            }
         }
-    }
-     
-    // if the left key is pushed, move to the next tab on the left
-    if(e.keyCode===keydownLeft){
-        tabFocus--
-        if (tabFocus < 0){
-            tabFocus=tabs.length-1;
+        // if the left key is pushed, move to the next tab on the left
+        else if (e.keyCode===keydownLeft){
+            tabFocus--
+            if (tabFocus < 0){
+                tabFocus=tabs.length-1;
+            }
         }
-    }
 
     tabs[tabFocus].setAttribute("tabsindex", 0);
     tabs[tabFocus].focus();
 
     console.log(tabFocus)
 }
+    }
+
+
+   
 
 function changeTabPanel(e) {
 
@@ -76,23 +77,39 @@ function changeTabPanel(e) {
 
     targetTab.setAttribute("aria-selected",true);
 
+    hideContent(mainContainer, `[role="tabpanel"]`);
+    showContent(mainContainer,[`#${targetPanel}`]);
 
-
-    mainContainer
+ /*    mainContainer
         .querySelectorAll(`[role="tabpanel"]`)
             .forEach((panel) => 
-                panel.setAttribute("hidden", true) );
+                panel.setAttribute("hidden", true) ); */
 
-    mainContainer.querySelector([`#${targetPanel}`]).removeAttribute(`hidden`);
+/*     mainContainer.querySelector([`#${targetPanel}`]).removeAttribute(`hidden`);
+ */
 
-    mainContainer
+    hideContent(mainContainer, `picture`);
+    showContent(mainContainer,[`#${targetImage}`]);
+
+   /*  mainContainer
     .querySelectorAll(`picture`)
         .forEach((pic) => 
-            pic.setAttribute("hidden", true) );
+            pic.setAttribute("hidden", true) ); */
 
-    mainContainer.querySelector([`#${targetImage}`]).removeAttribute(`hidden`);
+/*     mainContainer.querySelector([`#${targetImage}`]).removeAttribute(`hidden`);
+ */
+}
 
 
 
-console.log(mainContainer)
+function hideContent(parent, content) {
+    parent
+    .querySelectorAll(content)
+        .forEach((item) => 
+            item.setAttribute("hidden", true) );
+}
+
+
+function showContent(parent, content) {
+    parent.querySelector(content).removeAttribute(`hidden`)
 }
